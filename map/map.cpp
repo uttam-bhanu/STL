e55a228c3e5f53b18e53061b113d5099448047cc
uttam-bhanu <uttam4ever@gmail>
2018-11-23 20:21:57 +0530

    map prog

--- map/map.cpp
+++ map/map.cpp
@@ -1,1 +1,26 @@
+#include <map>
 
+using namespace std;
+
+int main()
+{
+    map<int, string> m;
+    
+    m.insert(pair<int,string> (1, "jhon"));
+    m.insert(pair<int,string> (2, "rahul"));
+    m.insert(pair<int,string> (3, "ravi"));
+    
+    map<int, string>::iterator itr;
+    
+    for(itr = m.begin(); itr != m.end(); itr++)
+       std::cout << itr->first<< " " << itr->second << std::endl;
+    
+    std::cout << "found value is" << std::endl;
+    
+    map<int, string>::iterator itr1;
+    itr1 = m.find(2);
+    std::cout << itr1->first<< " " << itr1->second << std::endl;
+    
+    
+    return 0;
+}
