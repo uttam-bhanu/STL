4e589b30e3e9e04689b432644fc4ab7d400428c7
uttam-bhanu <uttam4ever@gmail>
2018-11-23 16:47:40 +0530

    vector prog

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+   vector<int> v={ 2,4,4,6};
+   v.push_back(7);
+   v.push_back(9);
+   v.pop_back();
+   
+   vector<int>::iterator itr;
+   cout<< "using for iterator" << endl;
+   for(itr = v.begin();itr!= v.end(); itr++)
+      cout<<*itr<< endl;
+   cout<< "using for loop" << endl;
+   for (int x : v)
+     std::cout << x << std::endl;
 
+    return 0;
+}
